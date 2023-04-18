dca48b0569d7a6848b2add74fe67bf865a6436c9
Carolinne Moreira <carolinnemoreira@yahoo.com.br>
2023-04-17 22:19:52 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,15 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 18 Apr 2023 01:04:10 GMT
+ */
+
+module.exports = {
+  "ColorBlack": "#000000",
+  "ColorGray100": "#e1e1e6",
+  "ColorGray200": "#c4c4cc",
+  "ColorGray400": "#7c7c8a",
+  "ColorGray800": "#202024",
+  "ColorGray900": "#121214",
+  "ColorCyan300": "#9be1fb",
+  "ColorCyan500": "#81d8f7"
+};
